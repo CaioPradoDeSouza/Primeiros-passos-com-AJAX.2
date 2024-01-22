bf4609b99329ccd67fb7fcfc836435dceabdd9f0
Caio Prado de Souza <cpssouza7@gmail.com>
2024-01-21 22:05:37 -0300

    Atualização

---
+++ xhr.js
@@ -0,0 +1,16 @@
+var xhr = new XMLHttpRequest();
+var documento;
+
+xhr.responseType = "json"
+xhr.onreadystatechange = function (){
+    if(xhr.readyState == 4 && xhr.status == 200){
+        documento = xhr.response;
+        //documento = JSON.parse(documento);
+        console.log(documento);
+    }
+  
+}
+
+xhr.open("GET","https://jsonplaceholder.typicode.com/posts/1",true);
+
+xhr.send();
